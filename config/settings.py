ddced39ca83ccd52c932cfb739594df5311d0bf9
Palash Pingale <palashpingale135@gmail.com>
2026-03-12 02:13:08 +0530

    Add fallback defaults for optional env vars

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Read from environment variables
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-only-for-local')
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
-
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+EMAIL_BACKEND = os.environ.get(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.console.EmailBackend'
+)
 # Application definition
 
 INSTALLED_APPS = [
@@ -132,6 +134,5 @@ CELERY_RESULT_BACKEND = REDIS_URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
